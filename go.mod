3dcee9846edf0181f468112c68ee9d6e2b70ee50
tcwitte <tcwitte@users.noreply.github.com>
2022-12-23 20:05:50 +0000

    feat: minimal telemetry with Application Insights

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/tcwitte/go-azure-function
 
 go 1.19
+
+require github.com/microsoft/ApplicationInsights-Go v0.4.4
+
+require (
+	code.cloudfoundry.org/clock v0.0.0-20180518195852-02e53af36e6c // indirect
+	github.com/gofrs/uuid v3.3.0+incompatible // indirect
+)
--- handler.go
+++ handler.go
@@ -2,27 +2,38 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "os"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/microsoft/ApplicationInsights-Go/appinsights"
 )
 
+var telemetryClient = appinsights.NewTelemetryClient(os.Getenv("APPINSIGHTS_INSTRUMENTATIONKEY"))
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-    message := "This HTTP triggered function executed successfully. Pass a name in the query string for a personalized response.\n"
-    name := r.URL.Query().Get("name")
-    if name != "" {
-        message = fmt.Sprintf("Hello, %s. This HTTP triggered function executed successfully.\n", name)
-    }
-    fmt.Fprint(w, message)
+	defer func(start time.Time) {
+		telemetryClient.TrackRequest(r.Method, r.RequestURI, time.Since(start), "200")
+	}(time.Now())
+
+	message := "This HTTP triggered function executed successfully. Pass a name in the query string for a personalized response.\n"
+	name := r.URL.Query().Get("name")
+	if name != "" {
+		message = fmt.Sprintf("Hello, %s. This HTTP triggered function executed successfully.\n", name)
+	}
+	fmt.Fprint(w, message)
 }
 
 func main() {
+    defer appinsights.TrackPanic(telemetryClient, false)
+
     listenAddr := ":8080"
-    if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
-        listenAddr = ":" + val
-    }
-    http.HandleFunc("/api/HttpExample", helloHandler)
-    log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
-    log.Fatal(http.ListenAndServe(listenAddr, nil))
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		listenAddr = ":" + val
+	}
+	http.HandleFunc("/api/HttpExample", helloHandler)
+	log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
+	log.Fatal(http.ListenAndServe(listenAddr, nil))
 }
